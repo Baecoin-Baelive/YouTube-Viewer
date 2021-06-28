c48b2c702216b45453066b056b71534b40f1d5ef
Baecoin-Baelive <80746664+Baecoin-Baelive@users.noreply.github.com>
2021-06-28 19:40:18 +0700

    Add proxy list server

--- youtube_viewer.py
+++ youtube_viewer.py
@@ -308,6 +308,7 @@ def gather_proxy():
                  'https://raw.githubusercontent.com/TheSpeedX/SOCKS-List/master/socks4.txt',
                  'https://raw.githubusercontent.com/TheSpeedX/SOCKS-List/master/socks5.txt',
                  'https://raw.githubusercontent.com/ShiftyTR/Proxy-List/master/proxy.txt',
+                 'https://github.com/Baecoin-Baelive/freeproyxy/blob/main/proxy.txt',
                  'https://raw.githubusercontent.com/sunny9577/proxy-scraper/master/proxies.txt']
 
     for link in link_list:
